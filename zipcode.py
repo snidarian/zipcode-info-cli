557c76a2b3e94abea19eead28de16a3adabc2b5e
snidarian <cephalopod31956@gmail.com>
2021-06-28 10:04:57 -0400

    started country list dict

--- zipcode.py
+++ zipcode.py
@@ -10,13 +10,53 @@ import argparse
 # set up argparse
 parser = argparse.ArgumentParser(description="supply zip code argument to get information about the zipcode")
 
-args = parser.add_argument("Zipcode", help="zip code argument", type=str)
+args = parser.add_argument("zipcode", help="zip code argument", type=str)
+args = parser.add_argument('-', '--country', help='', type=str, default='us')
+args = parser.add_argument('-ls', '--list-countries', help='list country abbreviations')
 
 args = parser.parse_args()
 
-# 
-get_request_results = requests.get("https://api.zippopotam.us/us/90210")
 
+country_abbreviations = { 
+    'ad' : 'Andorra',
+    'ar' : 'Argentina',
+    'as' : 'American Samoa',
+    'at' : 'Austria',
+    '' : '',
+    '' : '',
+    '' : '',
+    '' : '',
+    '' : '',
+    '' : '',
+    '' : '',
+    '' : '',
+    '' : '',
+    '' : '',
+    '' : '',
+    '' : '',
+    '' : '',
+    '' : '',
+    '' : '',
+    '' : '',
+    '' : '',
+    '' : '',
+    '' : '',
+    '' : '',
+    '' : '',
+    '' : '',
+    '' : '',
+    '' : '',
+
+
+    }
+
+
+
+# Aquire json string response from Web API
+get_request_results = requests.get(f"https://api.zippopotam.us/{args.country}/{args.zipcode}")
+
+
+# Change from json string to python dictionary
 json_payload = get_request_results.json()
 
 
